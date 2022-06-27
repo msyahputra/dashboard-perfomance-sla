a33eb269364d510e79e6738bff1ad2cf9d55faf7
msyahputra <syahputram03@gmail.com>
2022-06-27 16:50:34 +0700

    menambahkan grafik di halaman kpi agent

--- app/Views/kpi_agent.php
+++ app/Views/kpi_agent.php
@@ -37,7 +37,7 @@
             <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
                 <div class="analytics-sparkle-line reso-mg-b-30">
                     <div class="analytics-content">
-                        <h5>Agent T1</h5>
+                        <h5>Voice</h5>
                         <h3><i class="fa fa-phone" aria-hidden="true"></i><span class="tuition-fees">Target <span class="text-warning counter">90</span>%</span></h3>
                         <span class="text-warning counter">94.20</span>%
                         <div class="progress m-b-0">
@@ -56,8 +56,8 @@
             <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
                 <div class="analytics-sparkle-line reso-mg-b-30">
                     <div class="analytics-content">
-                        <h5>Agent T2</h5>
-                        <h3><i class="fa fa-home" aria-hidden="true"></i><span class="tuition-fees">Target <span class="text-info counter">90</span>%</span></h3>
+                        <h5>IP</h5>
+                        <h3><i class="fa fa-archive" aria-hidden="true"></i><span class="tuition-fees">Target <span class="text-info counter">90</span>%</span></h3>
                         <span class="text-info counter">95.10</span>%
                         <div class="progress m-b-0">
                             <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="95.10" aria-valuemin="0" aria-valuemax="100" style="width:95.10%;"> <span class="sr-only">95.10% Complete</span> </div>
@@ -75,8 +75,8 @@
             <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
                 <div class="analytics-sparkle-line reso-mg-b-30 table-mg-t-pro dk-res-t-pro-30">
                     <div class="analytics-content">
-                        <h5>Agent T2 Violet</h5>
-                        <h3><i class="fa fa-code" aria-hidden="true"></i><span class="tuition-fees">Target <span class="text-info counter">95</span>%</span></h3>
+                        <h5>Datin</h5>
+                        <h3><i class="fa fa-code" aria-hidden="true"></i><span class="tuition-fees">Target <span class="text-info counter">90</span>%</span></h3>
                         <span class="text-info counter">99.64</span>%
                         <div class="progress m-b-0">
                             <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="99.64" aria-valuemin="0" aria-valuemax="100" style="width:99.64%;"> <span class="sr-only">99.64% Complete</span> </div>
@@ -94,8 +94,8 @@
             <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
                 <div class="analytics-sparkle-line table-mg-t-pro dk-res-t-pro-30">
                     <div class="analytics-content">
-                        <h5>Agent T2 Global</h5>
-                        <h3><i class="fa fa-globe" aria-hidden="true"></i><span class="tuition-fees">Target <span class="text-info counter">92</span>%</span></h3>
+                        <h5>Indihome</h5>
+                        <h3><i class="fa fa-home" aria-hidden="true"></i><span class="tuition-fees">Target <span class="text-info counter">90</span>%</span></h3>
                         <span class="text-info counter">91.18</span>%
                         <div class="progress m-b-0">
                             <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="91.18" aria-valuemin="0" aria-valuemax="100" style="width:91.18%;"> <span class="sr-only">91.18% Complete</span> </div>
@@ -116,8 +116,8 @@
             <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
                 <div class="analytics-sparkle-line reso-mg-b-30">
                     <div class="analytics-content">
-                        <h5>Agent Outbond Proaktif</h5>
-                        <h3><i class="fa fa-share-square" aria-hidden="true"></i><span class="tuition-fees">Target <span class="text-info counter">95</span>%</span></h3>
+                        <h5>Datin Violet</h5>
+                        <h3><i class="fa fa-exchange" aria-hidden="true"></i><span class="tuition-fees">Target <span class="text-info counter">90</span>%</span></h3>
                         <span class="text-info counter">88.14</span>%
                         <div class="progress m-b-0">
                             <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="88.14" aria-valuemin="0" aria-valuemax="100" style="width:88.14%;"> <span class="sr-only">88.14% Complete</span> </div>
@@ -135,8 +135,8 @@
             <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
                 <div class="analytics-sparkle-line reso-mg-b-30">
                     <div class="analytics-content">
-                        <h5>Agent Digital</h5>
-                        <h3><i class="fa fa-commenting-o" aria-hidden="true"></i><span class="tuition-fees">Target <span class="text-info counter">10</span>%</span></h3>
+                        <h5>Indihome Violet</h5>
+                        <h3><i class="fa fa-exchange" aria-hidden="true"></i><span class="tuition-fees">Target <span class="text-info counter">90</span>%</span></h3>
                         <span class="text-info counter">10</span>%
                         <div class="progress m-b-0">
                             <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100" style="width:10%;"> <span class="sr-only">93% Complete</span> </div>
@@ -154,8 +154,27 @@
             <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
                 <div class="analytics-sparkle-line reso-mg-b-30">
                     <div class="analytics-content">
-                        <h5>Imes</h5>
-                        <h3><i class="fa fa-caret-square-o-left" aria-hidden="true"></i><span class="tuition-fees">Target <span class="text-info counter">90</span>%</span></h3>
+                        <h5>Caring</h5>
+                        <h3><i class="fa fa-user-circle" aria-hidden="true"></i><span class="tuition-fees">Target <span class="text-info counter">90</span>%</span></h3>
+                        <span class="text-info counter">93.18</span>%
+                        <div class="progress m-b-0">
+                            <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="93.18" aria-valuemin="0" aria-valuemax="100" style="width:93.18%;"> <span class="sr-only">93.18% Complete</span> </div>
+                        </div>
+                        <span class="tuition-fees" style="color:#000; float: right; margin:20px 0px 0px 20px;">Last Update : 02-06-2022</span>
+                        <div class="box-content">
+                            <!-- <span class="post">T1 Voice</span> -->
+                            <ul class="icon">
+                                <li><a href="pencapaian-kpi-agent/imes">Click More <i class="fa fa-arrow-right" aria-hidden="true"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+                <div class="analytics-sparkle-line reso-mg-b-30">
+                    <div class="analytics-content">
+                        <h5>Global</h5>
+                        <h3><i class="fa fa-globe" aria-hidden="true"></i><span class="tuition-fees">Target <span class="text-info counter">90</span>%</span></h3>
                         <span class="text-info counter">93.18</span>%
                         <div class="progress m-b-0">
                             <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="93.18" aria-valuemin="0" aria-valuemax="100" style="width:93.18%;"> <span class="sr-only">93.18% Complete</span> </div>
@@ -171,9 +190,186 @@
                 </div>
             </div>
         </div>
+        <br>
+        <div class="row">
+            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+                <div class="analytics-sparkle-line reso-mg-b-30">
+                    <div class="analytics-content">
+                        <h5>Email</h5>
+                        <h3><i class="fa fa-envelope-o" aria-hidden="true"></i><span class="tuition-fees">Target <span class="text-warning counter">90</span>%</span></h3>
+                        <span class="text-warning counter">94.20</span>%
+                        <div class="progress m-b-0">
+                            <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="94.20" aria-valuemin="0" aria-valuemax="100" style="width:94.20%;"> <span class="sr-only">94.20% Complete</span> </div>
+                        </div>
+                        <span class="tuition-fees" style="color:#000; float: right; margin:20px 0px 0px 20px;">Last Update : 02-06-2022</span>
+                        <div class="box-content">
+                            <!-- <span class="post">T1 Voice</span> -->
+                            <ul class="icon">
+                                <li><a href="pencapaian-kpi-agent/agent-t1">Click More <i class="fa fa-arrow-right" aria-hidden="true"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+                <div class="analytics-sparkle-line reso-mg-b-30">
+                    <div class="analytics-content">
+                        <h5>Imes</h5>
+                        <h3><i class="fa fa-ticket" aria-hidden="true"></i><span class="tuition-fees">Target <span class="text-info counter">90</span>%</span></h3>
+                        <span class="text-info counter">95.10</span>%
+                        <div class="progress m-b-0">
+                            <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="95.10" aria-valuemin="0" aria-valuemax="100" style="width:95.10%;"> <span class="sr-only">95.10% Complete</span> </div>
+                        </div>
+                        <span class="tuition-fees" style="color:#000; float: right; margin:20px 0px 0px 20px;">Last Update : 02-06-2022</span>
+                        <div class="box-content">
+                            <!-- <span class="post">T1 Voice</span> -->
+                            <ul class="icon">
+                                <li><a href="pencapaian-kpi-agent/agent-t2">Click More <i class="fa fa-arrow-right" aria-hidden="true"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+                <div class="analytics-sparkle-line reso-mg-b-30 table-mg-t-pro dk-res-t-pro-30">
+                    <div class="analytics-content">
+                        <h5>Pro Aktif</h5>
+                        <h3><i class="fa fa-ticket" aria-hidden="true"></i><span class="tuition-fees">Target <span class="text-info counter">90</span>%</span></h3>
+                        <span class="text-info counter">99.64</span>%
+                        <div class="progress m-b-0">
+                            <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="99.64" aria-valuemin="0" aria-valuemax="100" style="width:99.64%;"> <span class="sr-only">99.64% Complete</span> </div>
+                        </div>
+                        <span class="tuition-fees" style="color:#000; float: right; margin:20px 0px 0px 20px;">Last Update : 02-06-2022</span>
+                        <div class="box-content">
+                            <!-- <span class="post">T1 Voice</span> -->
+                            <ul class="icon">
+                                <li><a href="pencapaian-kpi-agent/agent-t2-violet">Click More <i class="fa fa-arrow-right" aria-hidden="true"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+                <div class="analytics-sparkle-line table-mg-t-pro dk-res-t-pro-30">
+                    <div class="analytics-content">
+                        <h5>Social Media</h5>
+                        <h3><i class="fa fa-share-square" aria-hidden="true"></i><span class="tuition-fees">Target <span class="text-info counter">90</span>%</span></h3>
+                        <span class="text-info counter">91.18</span>%
+                        <div class="progress m-b-0">
+                            <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="91.18" aria-valuemin="0" aria-valuemax="100" style="width:91.18%;"> <span class="sr-only">91.18% Complete</span> </div>
+                        </div>
+                        <span class="tuition-fees" style="color:#000; float: right; margin:20px 0px 0px 20px;">Last Update : 02-06-2022</span>
+                        <div class="box-content">
+                            <!-- <span class="post">T1 Voice</span> -->
+                            <ul class="icon">
+                                <li><a href="pencapaian-kpi-agent/agent-t2-global">Click More <i class="fa fa-arrow-right" aria-hidden="true"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+                <div class="analytics-sparkle-line reso-mg-b-30">
+                    <div class="analytics-content">
+                        <h5>Chat</h5>
+                        <h3><i class="fa fa-commenting-o" aria-hidden="true"></i><span class="tuition-fees">Target <span class="text-warning counter">90</span>%</span></h3>
+                        <span class="text-warning counter">94.20</span>%
+                        <div class="progress m-b-0">
+                            <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="94.20" aria-valuemin="0" aria-valuemax="100" style="width:94.20%;"> <span class="sr-only">94.20% Complete</span> </div>
+                        </div>
+                        <span class="tuition-fees" style="color:#000; float: right; margin:20px 0px 0px 20px;">Last Update : 02-06-2022</span>
+                        <div class="box-content">
+                            <!-- <span class="post">T1 Voice</span> -->
+                            <ul class="icon">
+                                <li><a href="pencapaian-kpi-agent/agent-t1">Click More <i class="fa fa-arrow-right" aria-hidden="true"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
     </div>
 </div>
+<!-- Charts Start-->
+<div class="product-sales-area mg-tb-30">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="product-sales-chart">
+                    <div class="portlet-title">
+                        <div class="row">
+                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                <div class="caption pro-sl-hd">
+                                    <span class="caption-subject"><b>KPI AGENT</b></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <br>
+                    <figure class="highcharts-figure">
+                        <div id="trafic_kpi"></div>
+                    </figure>
+                </div>
+            </div>x
+        </div>
+    </div>
+</div>
+<script type="text/javascript">
+    Highcharts.chart('trafic_kpi', {
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Key Performance Indicator'
+        },
+        xAxis: {
+            categories: ['VOICE', 'IP', 'DATIN', 'INDIHOME', 'DATIN VIOLET', 'INDIHOME VIOLET', 'CARING', 'GLOBAL', 'EMAIL', 'IMES', 'PRO AKTIF', 'SOCIAL MEDIA', 'CHAT']
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: ''
+            }
+        },
+        legend: {
+            align: 'right',
+            x: -30,
+            verticalAlign: 'top',
+            y: 25,
+            floating: true,
+            borderWidth: 1,
 
-<!-- Static Table Start -->
-
+            backgroundColor: (
+                (Highcharts.theme && Highcharts.theme.legendBackgroundColor) ||
+                '#FFFFFF'),
+            shadow: true
+        },
+        plotOptions: {
+            column: {
+                stacking: 'normal',
+                dataLabels: {
+                    enabled: true
+                }
+            },
+            series: {
+                stacking: 'normal'
+            }
+        },
+        series: [{
+            name: 'Gap',
+            data: [6.08, 2.35, 5.02, 2.71, 1.14, 5.11, 3.41, 2.61, 1.24, 2.23, 3.33, 1.08, 0.92],
+            color: '#BF0B23'
+        }, {
+            name: 'Hasil',
+            data: [88.08, 89.35, 91.02, 95.71, 95.14, 95.11, 96.41, 95.61, 95.24, 99.23, 93.33, 93.08, 95.92]
+        }, {
+            name: 'Target',
+            data: [90.00, 90.00, 90.00, 90.00, 90.00, 90.00, 90.00, 90.00, 90.00, 90.00, 90.00, 90.00, 90.00]
+        }]
+    });
+</script>
 <?= $this->endSection(''); ?>
